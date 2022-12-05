fabf3f054349c4df45d8bbf38dfc3e026005d99c
cyboranf <cyboranf@gmail.com>
2022-12-06 00:12:21 +0100

    JavaFx with SpringBoot -configured 'test' classes -added

---
+++ src/main/java/com/example/MeetexApp/SimpleUiController.java
@@ -0,0 +1,30 @@
+package com.example.MeetexApp;
+
+import javafx.application.HostServices;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleUiController {
+
+    private final HostServices hostServices;
+
+    @FXML
+    public Label label;
+
+    @FXML
+    public Button button;
+
+
+    public SimpleUiController(HostServices hostServices) {
+        this.hostServices = hostServices;
+    }
+
+    @FXML
+    public void initialize () {
+        this.button.setOnAction(actionEvent -> this.label.setText(this.hostServices.getDocumentBase()));
+    }
+}
+
---
+++ src/test/java/com/example/MeetexApp/MeetexAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MeetexApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetexAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
