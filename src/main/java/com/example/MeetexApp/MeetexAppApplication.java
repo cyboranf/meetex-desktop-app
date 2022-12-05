3ac8fb7afac6a3f4f9e2c69f73df3601f05fbfff
cyboranf <cyboranf@gmail.com>
2022-12-06 00:10:38 +0100

    Project -opened

---
+++ src/main/java/com/example/MeetexApp/MeetexAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeetexApp;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetexAppApplication {
+
+	public static void main(String[] args) {
+		Application.launch(JavafxApplication.class, args);
+	}
+}
