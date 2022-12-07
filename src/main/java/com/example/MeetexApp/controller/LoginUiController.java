786ebe649e6d4ec20abc24b2b43fc6828f6cd6d6
cyboranf <cyboranf@gmail.com>
2022-12-07 17:46:45 +0100

    Login and Register views -added

---
+++ src/main/java/com/example/MeetexApp/controller/LoginUiController.java
@@ -0,0 +1,58 @@
+package com.example.MeetexApp.controller;
+
+import javafx.application.HostServices;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+
+@Controller
+public class LoginUiController {
+
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private Hyperlink createAccount;
+
+    private Stage stage;
+
+    private Scene scene;
+
+    private Parent root;
+
+
+    @FXML
+    public void switchToRegister(@NotNull ActionEvent event) throws IOException {
+
+            Parent root = FXMLLoader.load(getClass().getResource("registration.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setMinHeight(595);
+            stage.setMinWidth(325);
+            scene=new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+    }
+}
--- src/main/java/com/example/MeetexApp/controller/RegistrationUiController.java
+++ src/main/java/com/example/MeetexApp/controller/RegistrationUiController.java
@@ -1,4 +1,7 @@
 package com.example.MeetexApp.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class RegistrationUiController {
 }
