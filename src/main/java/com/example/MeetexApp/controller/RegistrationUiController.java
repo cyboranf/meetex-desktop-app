66daed4b89205a7415700c35e83a08fc905d0352
cyboranf <cyboranf@gmail.com>
2022-12-12 20:50:57 +0100

    Registration controller new method to create user -added

--- src/main/java/com/example/MeetexApp/controller/RegistrationUiController.java
+++ src/main/java/com/example/MeetexApp/controller/RegistrationUiController.java
@@ -1,25 +1,75 @@
 package com.example.MeetexApp.controller;
 
+import com.example.MeetexApp.domain.User;
+import com.example.MeetexApp.repository.UserRepository;
+import com.example.MeetexApp.service.UserService;
+import com.sun.tools.javac.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 
 @Controller
 public class RegistrationUiController {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
     private Stage stage;
 
     private Scene scene;
 
     private Parent root;
 
+
+    public void init() {
+
+    }
+
+
+    //byc moze trzeba bedzie zrobic jeszcze jeden widok afterRegister
+    @FXML
+    public void createUser(ActionEvent event) throws IOException {
+        if (firstName.getText().isEmpty()) {
+            emptyName.setText("First name is empty.");
+        }
+        if (lastName.getText().isEmpty()) {
+            emptySurname.setText("Last name is empty.");
+        }
+        if (email.getText().isEmpty()) {
+            emptyEmail.setText("Email is empty.");
+        }
+        if (password.getText().isEmpty()) {
+            emptyPassword.setText("Password is empty.");
+        }
+        if (matchingPassword.getText().isEmpty() || !matchingPassword.getText().equals(password.getText())) {
+            emptyMatchingPassword.setText("Passwords doesn't equals");
+        }
+        User user = new User();
+        user.setFirstName(firstName.getText());
+        user.setLastName(lastName.getText());
+        user.setEmail(email.getText());
+        user.setPassword(password.getText());
+        user.setMatchingPassword(matchingPassword.getText());
+        user.setRole("USER");
+
+        userRepository.save(user);
+        success.setText("success");
+
+    }
 
     @FXML
     public void switchToLogin(@NotNull ActionEvent event) throws IOException {
@@ -31,4 +81,32 @@ public class RegistrationUiController {
         stage.show();
 
     }
+
+    @FXML
+    private Label emptyName;
+    @FXML
+    private Label emptySurname;
+    @FXML
+    private Label emptyPassword;
+    @FXML
+    private Label emptyMatchingPassword;
+    @FXML
+    private Label emptyEmail;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField matchingPassword;
+
+    @FXML
+    private Label success;
+    @FXML
+    private Button signUp;
+
+
 }
