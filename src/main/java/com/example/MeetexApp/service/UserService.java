decd5443f3857bb1d92df8d11e7a083d5f3af854
cyboranf <cyboranf@gmail.com>
2022-12-12 20:51:26 +0100

    User service method to save user in database -implemented

--- src/main/java/com/example/MeetexApp/service/UserService.java
+++ src/main/java/com/example/MeetexApp/service/UserService.java
@@ -1,9 +1,20 @@
 package com.example.MeetexApp.service;
 
+import com.example.MeetexApp.domain.User;
+import com.example.MeetexApp.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
