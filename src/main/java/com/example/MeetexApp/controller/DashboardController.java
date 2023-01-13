1e37da067ab84a14f6ae614ed633a87d82f271f3
cyboranf <cyboranf@gmail.com>
2023-01-13 19:08:58 +0100

    Logout method -added

--- src/main/java/com/example/MeetexApp/controller/DashboardController.java
+++ src/main/java/com/example/MeetexApp/controller/DashboardController.java
@@ -1,5 +1,6 @@
 package com.example.MeetexApp.controller;
 
+import com.example.MeetexApp.domain.User;
 import com.example.MeetexApp.service.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 @Controller
@@ -38,22 +41,43 @@ public class DashboardController implements Initializable {
 
     @FXML
     public void openSignInView(ActionEvent event) throws IOException {
+        List<User> allUsers = userService.findAll();
+        for (User user : allUsers) {
+            user.setLogged(false);
+            userService.save(user);
+        }
+
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setControllerFactory(applicationContext::getBean);
-        fxmlLoader.setLocation(getClass().getResource("/registration.fxml"));
+        fxmlLoader.setLocation(getClass().getResource("/login.fxml"));
         stage.setScene(new Scene(fxmlLoader.load()));
         stage.show();
+    }
+
+    @FXML
+    public void logout(ActionEvent event) throws IOException {
+        User user = userService.findByLogged();
+        user.setLogged(false);
+        logoutLabel.setText("Logged out");
+    }
+
+    @FXML
+    public void dashboardView(ActionEvent event) throws IOException {
+        User user = userService.findByLogged();
+        firstName.setText(user.getFirstName());
 
     }
 
-
+    @FXML
+    private Label logoutLabel;
+    @FXML
+    private Label firstName;
     private Stage stage;
 
     private Scene scene;
 
     private Parent root;
-
 
 
     @Override
