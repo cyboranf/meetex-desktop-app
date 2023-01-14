93b1b33280763535f41f27f033bea58891c34b25
cyboranf <cyboranf@gmail.com>
2023-01-14 11:56:17 +0100

    Jpa methods with implementation -added

--- src/main/java/com/example/MeetexApp/repository/NotificationRepository.java
+++ src/main/java/com/example/MeetexApp/repository/NotificationRepository.java
@@ -1,7 +1,12 @@
 package com.example.MeetexApp.repository;
 
 import com.example.MeetexApp.domain.Notification;
+import com.example.MeetexApp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    @Query(value = "SELECT count(n) from notification n WHERE n.toUser = :us", nativeQuery = true)
+    Integer countOfNotification(@Param("us") User us);
 }
--- src/main/java/com/example/MeetexApp/service/NotificationService.java
+++ src/main/java/com/example/MeetexApp/service/NotificationService.java
@@ -1,9 +1,20 @@
 package com.example.MeetexApp.service;
 
+import com.example.MeetexApp.domain.User;
+import com.example.MeetexApp.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class NotificationService {
+    private final NotificationRepository notificationRepository;
+
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    public Integer countOfNot(User user) {
+        return notificationRepository.countOfNotification(user);
+    }
 }
