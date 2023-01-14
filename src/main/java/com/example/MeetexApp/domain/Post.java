a3f86df516117dd48c27c41133206c1a3d956043
cyboranf <cyboranf@gmail.com>
2023-01-14 09:45:18 +0100

    title to post model -added

--- src/main/java/com/example/MeetexApp/domain/Post.java
+++ src/main/java/com/example/MeetexApp/domain/Post.java
@@ -15,6 +15,8 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title")
+    private String title;
     @Column(name = "text")
     private String text;
 
