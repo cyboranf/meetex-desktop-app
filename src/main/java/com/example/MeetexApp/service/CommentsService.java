a5d2864db7e1a944331b30c08c7995e9bd7ed272
cyboranf <cyboranf@gmail.com>
2023-01-16 16:30:32 +0100

    implementation of jpa methods -added

--- src/main/java/com/example/MeetexApp/service/CommentsService.java
+++ src/main/java/com/example/MeetexApp/service/CommentsService.java
@@ -1,9 +1,20 @@
 package com.example.MeetexApp.service;
 
+import com.example.MeetexApp.domain.Comments;
+import com.example.MeetexApp.repository.CommentsRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class CommentsService {
+    private final CommentsRepository commentsRepository;
+
+    public CommentsService(CommentsRepository commentsRepository) {
+        this.commentsRepository = commentsRepository;
+    }
+
+    public Comments save(Comments comments){
+        return commentsRepository.save(comments);
+    }
 }
