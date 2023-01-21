870da385d564b6e1ab09745ed2e08e0029fda792
cyboranf <cyboranf@gmail.com>
2023-01-21 19:13:22 +0100

    Initializer with new method in PostService -added

--- src/main/java/com/example/MeetexApp/initializer/FriendsInitializer.java
+++ src/main/java/com/example/MeetexApp/initializer/FriendsInitializer.java
@@ -74,7 +74,7 @@ public class FriendsInitializer {
             post3.setReactions(0);
             post3.setText("This is first post from admin3, welcome to Meetex");
             post3.setTitle("Title of first post from admin3");
-            post3.setSender(user);
+            post3.setSender(user3);
             post3.setReactions(0);
             post3.setAddressee(user3.getFriends());
             postService.save(post3);
@@ -88,13 +88,94 @@ public class FriendsInitializer {
             userService.save(user);
         } else {
             newFriends.clear();
-            newFriends.add(userService.findByEmail("admin@admin.com").get(0));
-            newFriends.add(userService.findByEmail("admin2@admin.com").get(0));
-            newFriends.add(userService.findByEmail("admin3@admin.com").get(0));
+            user1.setEmail("admin4@admin.com");
+            user1.setFirstName("admin4");
+            user1.setLastName("admin4");
+            userService.save(user1);
+
+            post1.setReactions(0);
+            post1.setText("This is first post from admin4, welcome to Meetex");
+            post1.setTitle("Title of first post from admin4");
+            post1.setSender(user1);
+            post1.setReactions(0);
+            post1.setAddressee(user1.getFriends());
+            post1.setSendDate(date);
+            postService.save(post1);
+
+
+            user2.setEmail("admin6@admin.com");
+            user2.setFirstName("admin6");
+            user2.setLastName("admin6");
+            userService.save(user2);
+
+            post2.setReactions(0);
+            post2.setText("This is first post from admin6, welcome to Meetex");
+            post2.setTitle("Title of first post from admin6");
+            post2.setSender(user2);
+            post2.setReactions(0);
+            post2.setSendDate(date);
+            post2.setAddressee(user2.getFriends());
+            postService.save(post2);
+
+
+            user3.setEmail("admin6@admin.com");
+            user3.setFirstName("admin6");
+            user3.setLastName("admin6");
+            userService.save(user3);
+
+            post3.setSendDate(date);
+            post3.setReactions(0);
+            post3.setText("This is first post from admin3, welcome to Meetex");
+            post3.setTitle("Title of first post from admin3");
+            post3.setSender(user3);
+            post3.setReactions(0);
+            post3.setAddressee(user3.getFriends());
+            postService.save(post3);
+
+            newFriends.add(user1);
+            newFriends.add(user2);
+            newFriends.add(user3);
 
             user.setFriends(newFriends);
             user.setFriendsCount(3);
             userService.save(user);
+//            newFriends.clear();
+//            newFriends.add(userService.findByEmail("admin@admin.com").get(0));
+//            newFriends.add(userService.findByEmail("admin2@admin.com").get(0));
+//            newFriends.add(userService.findByEmail("admin3@admin.com").get(0));
+//
+//            List<Post> allPosts = postService.findAll();
+//            //tu pokombinowac zeby jak ktos usunie posty od adminow to byly generowane od poczatku
+//            post1.setReactions(0);
+//            post1.setText("This is first post from admin1, welcome to Meetex");
+//            post1.setTitle("Title of first post from admin1");
+//            post1.setSender(userService.findByEmail("admin@admin.com").get(0));
+//            post1.setReactions(0);
+//            post1.setAddressee(user1.getFriends());
+//            post1.setSendDate(date);
+//            postService.save(post1);
+//
+//            post2.setReactions(0);
+//            post2.setText("This is first post from admin2, welcome to Meetex");
+//            post2.setTitle("Title of first post from admin2");
+//            post2.setSender(userService.findByEmail("admin2@admin.com").get(0));
+//            post2.setReactions(0);
+//            post2.setSendDate(date);
+//            post2.setAddressee(user2.getFriends());
+//            postService.save(post2);
+//
+//            post3.setSendDate(date);
+//            post3.setReactions(0);
+//            post3.setText("This is first post from admin3, welcome to Meetex");
+//            post3.setTitle("Title of first post from admin3");
+//            post3.setSender(userService.findByEmail("admin3@admin.com").get(0));
+//            post3.setReactions(0);
+//            post3.setAddressee(user3.getFriends());
+//            postService.save(post3);
+//
+//            user.setFriends(newFriends);
+//            user.setFriendsCount(3);
+//            userService.save(user);
         }
 
 
--- src/main/java/com/example/MeetexApp/service/PostService.java
+++ src/main/java/com/example/MeetexApp/service/PostService.java
@@ -4,19 +4,24 @@ import com.example.MeetexApp.domain.Post;
 import com.example.MeetexApp.domain.User;
 import com.example.MeetexApp.repository.PostRepository;
 import com.example.MeetexApp.repository.UserRepository;
-import javafx.geometry.Pos;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
 @Transactional
 public class PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
-    public PostService(UserRepository userRepository, PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserRepository userRepository1) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository1;
     }
 
     public List<Post> findByUserId(User user) {
@@ -33,5 +38,31 @@ public class PostService {
 
     public void save(Post post) {
         postRepository.save(post);
+    }
+
+    @Transactional
+    public List<Post> findPostToShow(User user) {
+        List<Post> posts = postRepository.findAll();
+        List<Post> returnedList = new ArrayList<>();
+        List<User> friendsList = user.getFriends();
+        for (int i = posts.size() - 1; i >= 0; i--) {
+            if (posts.get(i).getSender().getId().equals(user.getId())) {
+                returnedList.add(posts.get(i));
+            } else {
+                for (int j = 0; j < friendsList.size(); j++) {
+                    if (posts.get(i).getSender().getId().equals(friendsList.get(j).getId())) {
+                        returnedList.add(posts.get(i));
+                    }
+                }
+            }
+        }
+
+        Collections.sort(returnedList, new Comparator<Post>() {
+            @Override
+            public int compare(Post post, Post post2) {
+                return post.getSendDate().compareTo(post2.getSendDate());
+            }
+        });
+        return returnedList;
     }
 }
